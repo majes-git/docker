2130e46b5aacfb2afe4a4c24a8d0eaaf3f5c62ba
Mathias Jeschke <mj@majes.de>
2022-12-04 21:31:43 +0100

    Added inotify monitoring for authorize file and radiusd reload

--- 8021x-freeradius/entrypoint.sh
+++ 8021x-freeradius/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+# if dh file is empty, generate new parameters
+DH_FILE=/etc/raddb/certs/dh
+if [ ! -s $DH_FILE ]; then
+    openssl dhparam -out $DH_FILE 1024
+fi
+
+# TODO: add sanity checks for TLS certs/key
+
+# run incrond to monitor changes on authorize file and to reload radiusd
+incrond
+
 exec radiusd -f
