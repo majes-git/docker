0e45eb4005b43db690c65646c52a6bfbfacceb3f
Mathias Jeschke <mj@majes.de>
2022-12-21 02:29:03 +0100

    Added minimal wireguard container based on alpine

---
+++ wireguard-alpine/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# init
+ip link add wg0 type wireguard
+ip address add dev wg0 ${IP_ADDRESS}
+echo "${PRIVATE_KEY}" > /private_key
+chmod 0400 /private_key
+
+# prepare peers
+PEERS_COMMAND=""
+for PEER in ${PEERS}; do
+    ADDRESS=$(echo ${PEER} | awk -F: '{ print $1 }')
+    PUBLIC_KEY=$(echo ${PEER} | awk -F: '{ print $2 }')
+    PEERS_COMMAND="${PEERS_COMMAND} peer ${PUBLIC_KEY} allowed-ips ${ADDRESS}"
+    if [ -n "${KEEPALIVE}" ]; then PEERS_COMMAND="${PEERS_COMMAND} persistent-keepalive ${KEEPALIVE}"; fi
+    if [ -n "${ENDPOINT}" ]; then PEERS_COMMAND="${PEERS_COMMAND} endpoint ${ENDPOINT}"; break; fi
+done
+
+# set up wireguard environment
+wg set wg0 private-key /private_key ${PEERS_COMMAND}
+${IPTABLES}
+
+# do not stop the container when script is done
+sleep infinity
+
