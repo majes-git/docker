e0e428a6fe14de0e6ecfa07c9c4133c7312bc779
Mathias Jeschke <mj@majes.de>
2025-07-09 23:17:33 +0200

    Fixed rsync path

--- wireguard-ui-multi/data-sync/entrypoint.sh
+++ wireguard-ui-multi/data-sync/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_PATH=/srv/docker/containers/majes-git-docker/wireguard-ui-multi/data/
+TARGET_PATH=/srv/docker/containers/majes-git-docker/wireguard-ui-multi/
 
 while true; do
     inotifywait -r -e create -e modify /data /sites.yaml
@@ -8,5 +8,5 @@ while true; do
         mkdir -p /tmp/$(dirname $config)
         sed -e 's/172.30/172.31/' $config > /tmp/$config
     done
-    rsync -avz -e "ssh -p $SYNC_SSH_PORT" /tmp/data/ /sites.yaml $SYNC_TARGET:$TARGET_PATH
+    rsync -avz -e "ssh -p $SYNC_SSH_PORT" /tmp/data /sites.yaml $SYNC_TARGET:$TARGET_PATH
 done
