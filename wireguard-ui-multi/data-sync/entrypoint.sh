666b59e1ce082db83aa6ba27fd65ccc23670f970
Mathias Jeschke <mj@majes.de>
2025-07-09 23:03:51 +0200

    Improvements for vpn1 -> vpn2 sync

---
+++ wireguard-ui-multi/data-sync/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TARGET_PATH=/srv/docker/containers/majes-git-docker/wireguard-ui-multi/data/
+
+while true; do
+    inotifywait -r -e create -e modify /data /sites.yaml
+    for config in /data/*/config.json; do
+        mkdir -p /tmp/$(dirname $config)
+        sed -e 's/172.30/172.31/' $config > /tmp/$config
+    done
+    rsync -avz -e "ssh -p $SYNC_SSH_PORT" /tmp/data/ /sites.yaml $SYNC_TARGET:$TARGET_PATH
+done
