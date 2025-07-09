a56f00a5fc36715aa28d51e9c8ac03a958862991
Mathias Jeschke <mj@majes.de>
2025-07-09 16:56:24 +0200

    Added local_options to override host specific settings

--- wireguard-ui-multi/generate_sites.py
+++ wireguard-ui-multi/generate_sites.py
@@ -17,19 +17,33 @@ def load_sites(filename='sites.yaml'):
         return yaml.safe_load(fd)
 
 
+def load_local_options(filename='local_options.yaml'):
+    local_options = {
+        'vpn': 'vpn1',
+        'vpn_net': '172.30.{}.0/24',
+        'tls': True,
+    }
+    try:
+        with open(filename) as fd:
+            local_options.update(yaml.safe_load(fd))
+    except FileNotFoundError:
+        pass
+    return local_options
+
+
 def load_template(filename='docker-compose.template'):
     with open(filename) as fd:
         return Template(fd.read())
 
 
-def populate_site_parameters(site, domain='example.com', vpn='vpn1'):
+def populate_site_parameters(site, domain, vpn, vpn_net):
     id = site['id']
     site_name = site['name']
     site.update({
       'image': IMAGE,
       'vpn_endpoint': f'{site_name}.{vpn}.{domain}',
       'vpn_port': '518{:02d}'.format(id),
-      'vpn_net': f'172.30.{id}.0/24',
+      'vpn_net': vpn_net.format(id),
     })
     return site
 
@@ -51,14 +65,21 @@ def main():
     sites = parameters.get('sites')
     if not sites:
         error('sites.yaml does not contain a site')
+    local_options = load_local_options()
 
     append = False
     for site in parameters.get('sites'):
         print(site.get('name'))
-        site = populate_site_parameters(site, domain)
+        site = populate_site_parameters(site, domain, local_options['vpn'], local_options['vpn_net'])
         site_config = template.safe_substitute(**site)
         # if no extra net is configured (tunnelbroker mode), remove variable
         site_config = site_config.replace(',$net', '')
+        if not local_options['tls']:
+            site_config_lines = site_config.splitlines()
+            for line in site_config_lines.copy():
+                if 'tls' in line:
+                    site_config_lines.remove(line)
+            site_config = '\n'.join(site_config_lines)
         write_site_config(site_config, append)
         append = True
 
